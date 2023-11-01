8e62f15fb0ae81f41bc22477b074f92bfdd39d04
Dion Santana <dion.santana@gmail.com>
2023-11-01 15:05:04 -0400

    Hello world form

---
+++ src/JudiciaryForm.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Form from 'react-jsonschema-form';
+import { JSONSchema7 } from "json-schema";
+import { RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv8';
+
+type JudiciaryBoardProps = {}
+
+const schema: RJSFSchema = {
+  title: 'Todo',
+  type: 'object',
+  required: ['title'],
+  properties: {
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false },
+  },
+}
+
+const log = (type) => console.log.bind(console, type);
+
+export const JudiciaryForm = ({}: JudiciaryBoardProps) => {
+  return (
+    <>
+      <h3>Judiciary Form</h3>
+      <Form 
+        schema={schema}
+        validator={validator}
+        onChange={log('changed')}
+        onSubmit={log('submitted')}
+        onError={log('errors')} 
+      />
+    </>
+  )
+} 
