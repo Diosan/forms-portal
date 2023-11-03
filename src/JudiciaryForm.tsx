8c2f838680d3a3e418c39ca395467fac395aaee6
Dion Santana <dion.santana@gmail.com>
2023-11-02 20:09:10 -0400

    get schema from backend

--- src/JudiciaryForm.tsx
+++ src/JudiciaryForm.tsx
@@ -1,22 +1,30 @@
 // Import Form and validator from RJSF form despite what documentation says or fails to say
+import {useEffect, useState} from "react";
 import Form from 'react-jsonschema-form';
 import validator from '@rjsf/validator-ajv8';
+import axios from "axios";
 
 type JudiciaryBoardProps = {}
 
-const schema = {
-  title: 'Todo',
-  type: 'object',
-  required: ['title'],
-  properties: {
-    title: { type: 'string', title: 'Title', default: 'A new task' },
-    done: { type: 'boolean', title: 'Done?', default: false },
-  },
-}
+
+
+
+
 
 const log = (type: any) => console.log.bind(console, type);
 
 export const JudiciaryForm = ({}: JudiciaryBoardProps) => {
+
+  const [schema, setSchema] = useState({});
+  
+  useEffect(() => {
+    axios.get('http://localhost:3000/schema')
+    .then((response) => {
+      console.log(response.data);
+      setSchema(response.data);
+    })
+  }, []);
+
   return (
     // Typescript schema assignment error does not prevent porper operation of RJSF form
     <>
@@ -24,7 +32,7 @@ export const JudiciaryForm = ({}: JudiciaryBoardProps) => {
       <Form 
         schema={schema}
         validator={validator}
-        onChange={log('changed')}
+        // onChange={log('changed')}
         onSubmit={log('submitted')}
         onError={log('errors')} 
       />
