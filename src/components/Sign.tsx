9a89e08a996702cbf9b32a9715962b553cc2780e
hilwyn <hilwyn@gmail.com>
2023-12-07 06:28:46 -0500

    commit

--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -1,6 +1,6 @@
 // Import Form and validator from RJSF form despite what documentation says or fails to say
-import {useEffect, useState} from "react"
-import { API_URL} from "../config/api"
+import { useEffect, useState } from "react"
+import { API_URL } from "../config/api"
 import axios from "axios"
 import { RJSFSchema, UiSchema } from '@rjsf/utils'
 import Form from 'react-jsonschema-form'
@@ -20,7 +20,7 @@ import {
     Route,
     Routes,
     useParams,
-  } from "react-router-dom"
+} from "react-router-dom"
 
 import { SignIndictment } from "./SignIndictment"
 
@@ -32,7 +32,7 @@ type SignProps = {}
 
 const log = (type: any) => console.log.bind(console, type)
 
-export const Sign = ({}: SignProps) => {
+export const Sign = ({ }: SignProps) => {
 
     const { id } = useParams()
 
@@ -40,7 +40,7 @@ export const Sign = ({}: SignProps) => {
 
     const auth = new AuthService
 
-    
+
 
     const [status, setStatus] = useState('')
     const [complainantName, setComplainantName] = useState('')
@@ -50,8 +50,8 @@ export const Sign = ({}: SignProps) => {
     const [title, setTitle] = useState('')
     const [accuseds, setAccuseds] = useState([])
     const [offences, setOffences] = useState<{
-        id: any 
-        firstName: any 
+        id: any
+        firstName: any
         lastName: any
         ICCS: any
         dateOfOffence: any
@@ -89,7 +89,7 @@ export const Sign = ({}: SignProps) => {
             let charges = await axios.get(API_URL + '/api/accuseds/charges/' + accused.id)
 
             charges.data.charges.map((charge: any, i: number) => {
-                
+
                 let offence = {
                     id: charge.id,
                     firstName: firstName,
@@ -103,9 +103,9 @@ export const Sign = ({}: SignProps) => {
             })
 
         }
-        
-      
-        if(auth.loggedIn()) {
+
+
+        if (auth.loggedIn()) {
 
             fetchSubmission()
 
@@ -113,35 +113,15 @@ export const Sign = ({}: SignProps) => {
             // navigate("/")
         }
 
-    },[])
+    }, [])
 
     return (
         <div className="d-flex">
 
-<div className="pt-3 px-3 mx-3" 
-                    style={{maxWidth:"250px", 
-                    flexShrink:0, position:"fixed", top:0, left:0, height:"100%"}}
-            >
-                    <div   className="mb-3">
-                        <a  className="mb-4" href="#">
-                            <img src="/jsswf-01.svg" width="90"  className="d-inline-block align-top " alt=""/>
-                        </a>
-                    </div>
-                   
-                    <div  className="text-center " style={{}}>
-                        <ul className=" m-0 navbar-nav ms-auto">
-                            
-                            <li  className="nav-item active jud-header-item"><a  className="nav-link" href="/">Home  </a></li>
-                            <li  className="nav-item jud-header-item"><a  className="nav-link" href="/submissions"> My Submissions</a></li>
-                            <li  className="nav-item jud-header-item"><a  className="nav-link" href="/submission"> New Complaint With Oath</a></li>
-                            <li  className="nav-item jud-header-item"><a  className="nav-link" href="/indictable"> New Indictable</a></li>
-                            <li  className="nav-item jud-header-item"><button  style={{width:"100%"}}  className="nav-link m-0 text-center">Logout</button></li>
-                        </ul>
-                    </div>
-            </div>
+            
 
-            <div className="container submissions-container" style={{ borderRadius:"5px", maxWidth: "900px", padding: "20px 40px", margin: "30px 30px 30px 300px", flexGrow:1 }}>
-                
+            <div className="container submissions-container" style={{ borderRadius: "5px", maxWidth: "900px", padding: "20px 40px", margin: "30px 30px 30px 300px", flexGrow: 1 }}>
+
 
                 <div className="fade show">
                     <h4 className='mb-3'> Complaint With Oath ({title})</h4>
@@ -149,15 +129,15 @@ export const Sign = ({}: SignProps) => {
 
                 <div className="card fade show">
                     <div className="card-body">
-                        <h5 className="card-title">Complainant: The State</h5><br/> <br/>
+                        <h5 className="card-title">Complainant: The State</h5><br /> <br />
 
                         <div className="text-left complainant-details">
-                            { accuseds.map((accused: any, i: number) => (
+                            {accuseds.map((accused: any, i: number) => (
                                 <table className="accused-table" key={accused.id}>
                                     <tbody>
                                         <tr>
                                             <td><label>Name of Accused: </label></td>
-                                            <td>{i+1} {accused.firstName} {accused.lastName} <strong>-{accused.adulthood}</strong></td>
+                                            <td>{i + 1} {accused.firstName} {accused.lastName} <strong>-{accused.adulthood}</strong></td>
                                         </tr>
                                         <tr>
                                             <td><label>ID: </label></td>
@@ -175,7 +155,7 @@ export const Sign = ({}: SignProps) => {
                                             <td><label>Address: </label></td>
                                             <td>{accused.address}</td>
                                         </tr>
-                                        <tr><td><br/></td></tr>
+                                        <tr><td><br /></td></tr>
                                         <tr><td></td></tr>
                                         <tr>
                                             <td><label>National of Trinidad and Tobago: </label></td>
@@ -194,13 +174,13 @@ export const Sign = ({}: SignProps) => {
                                             <td>{accused.previousCriminalRecord}</td>
                                         </tr>
                                     </tbody>
-                                </table>                                
+                                </table>
                             ))}
 
                         </div>
-                        <br/><br/>
-                        
-                        <h5 className="card-title">Offences</h5><br/>
+                        <br /><br />
+
+                        <h5 className="card-title">Offences</h5><br />
                         <div className="text-left complainant-details">
                             <table className="offence-table" >
                                 <thead>
@@ -224,8 +204,8 @@ export const Sign = ({}: SignProps) => {
                                             </tr>                                        
                                        
                                     ))} */}
-                                    { accuseds.map((accused: any) => (
-                                        <Offences 
+                                    {accuseds.map((accused: any) => (
+                                        <Offences
                                             first_name={accused.firstName}
                                             last_name={accused.lastName}
                                             accused_id={accused.id}
@@ -236,23 +216,23 @@ export const Sign = ({}: SignProps) => {
                             </table>
                         </div>
 
-                        <br/><br/>
+                        <br /><br />
                         <div className="text-left complainant-details">
                             <p>I <strong>{complainantName}</strong> Police Constable No. <strong>{complainantRegNum}</strong>, hereby swear by affixing my signature to this declaration, that I make this complaint conscientiously having reasonable grounds for believing that  the named accused person has committed the offence alleged and stated in the complaint and that the particulars are true to the best of my knowledge
                             </p>
-                            <p><strong>{' ' + currentDate() }</strong></p>
-                            <br/><br/><br/>
+                            <p><strong>{' ' + currentDate()}</strong></p>
+                            <br /><br /><br />
                             {status == 'signed' ?
-                                    <div className="signature-frame">
-                                        <strong>Signed by: {complainantEmail}</strong>
-                                    </div>
+                                <div className="signature-frame">
+                                    <strong>Signed by: {complainantEmail}</strong>
+                                </div>
                                 :
-                                    <SignIndictment submission_id={parseInt('' + id)} complainant_email={complainantEmail} />
+                                <SignIndictment submission_id={parseInt('' + id)} complainant_email={complainantEmail} />
                             }
-                            
+
                             {/* <a className="btn btn-secondary float-end" onClick={signSubmission} >Sign</a> */}
                         </div>
-                    
+
                     </div>
 
 
@@ -262,7 +242,7 @@ export const Sign = ({}: SignProps) => {
 
 
             </div>
-        
+
         </div>
     )
 
