fad6ef48f5f1014f71953ba2887e8f4ee9257c7e
Dion Santana <dion.santana@gmail.com>
2023-12-11 16:42:40 -0400

    verfied in submissions component

--- src/components/Submissions.tsx
+++ src/components/Submissions.tsx
@@ -268,6 +268,26 @@ export const Submissions = ({ }: SubmissionsProps) => {
                                         </div>
                                     )}
 
+
+                                    {groupedSubmissions['verified'] && (
+                                        <div className="mt-4 group-submission complete-group" style={{ backgroundColor: '#fff' }}>
+                                            <h4 className="my-2 mb-4">Signed Submissions</h4>
+                                            {groupedSubmissions['verified'].map((submission:any) => (
+                                                <a
+                                                    href={'/view/' + submission.id}
+                                                    key={submission.id}
+                                                >
+                                                    <div className="card submission-card" style={{ padding: '1px' }}>
+                                                        <div className="card-body" style={{ padding: '10px 25px' }}>
+                                                            <h5 className="card-title">{submission.description}</h5>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            ))}
+                                        </div>
+                                    )}
+
+
                                 </div>
                                 
 
