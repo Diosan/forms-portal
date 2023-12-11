45abb6cd538a42e9697bd4b452a5849d06ef1f85
Dion Santana <dion.santana@gmail.com>
2023-12-11 12:32:11 -0400

    view signatures

--- src/components/Signatures.tsx
+++ src/components/Signatures.tsx
@@ -28,6 +28,140 @@ export const Signatures = ({
 
     const navigate = useNavigate()
 
-    return (<><h5>Signatures</h5></>)
+    const [otpSent, setOtpSent] = useState(false)
+    const [verifyOtpSent, setVerifyOtpSent] = useState(false)
+    const [signOTP, setSignOTP] = useState('')
+    const [signed, setSigned] = useState(false)
+    const [verified, setVerified] = useState(false)
+    const [verifierEmail, setVerifierEmail] = useState('')
+    const [signatureHash, setSignatureHash] = useState('')
+    const [signatureName, setSignatureName] = useState('')
+    const [signatureDate, setSignatureDate] = useState('')
+    const [verificationHash, setVerificationHash] = useState('')
+    const [verificationName, setVerificationName] = useState('')
+    const [verificationDate, setVerificationDate] = useState('')
+
+    useEffect(() => {
+        (async () => {
+            
+            let submission = await axios.get(API_URL + '/api/submissions/' + submission_id)
+            if(submission.data.submission.status == 'signed' || submission.data.submission.status == 'verified') {
+
+                let retrieved_signature = await axios.post(
+                    API_URL + '/api/submissions/signature/', 
+                    {                                                                        
+                        submission_id: submission.data.submission.id,
+                        userId: submission.data.submission.userId
+                    }
+                )
+
+                console.log('\n\n\n Signature for signed submission: ', retrieved_signature.data)
+                setSignatureHash(retrieved_signature.data.signature.hash)
+                setSignatureName(retrieved_signature.data.user.firstName + ' ' + retrieved_signature.data.user.lastName)                
+                setSignatureDate(retrieved_signature.data.signature.createdAt)
+
+            }
+
+            if(submission.data.submission.status == 'verified') {
+
+                // let retrieved_signature = await axios.post(
+                //     API_URL + '/api/submissions/signature/', 
+                //     {                                                                        
+                //         submission_id: submission.data.submission.id,
+                //         userId: submission.data.submission.userId
+                //     }
+                // )
+
+                // console.log('\n\n\n Signature for signed submission: ', retrieved_signature.data)
+                // setSignatureHash(retrieved_signature.data.signature.hash)
+                // setSignatureName(retrieved_signature.data.user.firstName + ' ' + retrieved_signature.data.user.lastName)                
+                // setSignatureDate(retrieved_signature.data.signature.createdAt)
+
+
+                let retrieved_verification = await axios.post(
+                    API_URL + '/api/submissions/verification/', 
+                    {                                                                        
+                        submission_id: submission.data.submission.id,
+                        userId: submission.data.submission.userId
+                    }
+                )
+
+                console.log('\n\n\n Verification for signed submission: ', retrieved_verification.data)
+                setVerificationHash(retrieved_verification.data.signature.hash)
+                setVerificationName(retrieved_verification.data.user.firstName + ' ' + retrieved_verification.data.user.lastName)                
+                setVerificationDate(retrieved_verification.data.signature.createdAt)
+
+            }
+
+            
+
+        })();        
+    }, [])
+
+    return (
+        <>
+            <h5>Signatures</h5>
+
+            { (already_signed || already_verified) ?
+                <div className="signature-format-complainant">
+                    {/* <!-- Row 1 --> */}
+                    <div className="col-12 col">
+                        <div className="logo-placeholder d-flex swf-sign">
+                            <span className="swf-e-signed">e-signed on</span> <span className="swf-swif">SWiF</span>
+                        </div>
+                    </div>
+                    <div className="col-12 col">
+                        <div className="text-placeholder text-right">{signatureDate.substring(11, 20)}</div>
+                        <div className="text-placeholder text-right">{signatureDate.substring(0, 10)}</div>
+                        {/* <div className="text-placeholder text-right">[ip address]</div> */}
+                    </div>
+
+                    {/* <!-- Row 2 --> */}
+                    <div className="col-12 col">
+                        <div className="name-placeholder fw-bold  text-left">{signatureName}</div>
+                    </div>
+
+                    {/* <!-- Row 3 --> */}
+                    <div className="col-12 col">
+                        <p className="hash-placeholder text-left">
+                        {signatureHash}</p>
+                    </div>
+                </div>
+                :
+                <></>
+            }
+
+            { already_verified?
+                <div className="signature-format-verifier">
+                    {/* <!-- Row 1 --> */}
+                    <div className="col-12 col">
+                        <div className="logo-placeholder d-flex swf-sign">
+                            <span className="swf-e-signed">Verified on </span> <span className="swf-swif">SWiF</span>
+                        </div>
+                    </div>
+                    <div className="col-12 col">
+                        <div className="text-placeholder text-right">{verificationDate.substring(11, 20)}</div>
+                        <div className="text-placeholder text-right">{verificationDate.substring(0, 10)}</div>
+                        {/* <div className="text-placeholder text-right">[ip address]</div> */}
+                    </div>
+
+                    {/* <!-- Row 2 --> */}
+                    <div className="col-12 col">
+                        <div className="name-placeholder fw-bold  text-left">{verificationName}</div>
+                    </div>
+
+                    {/* <!-- Row 3 --> */}
+                    <div className="col-12 col">
+                        <p className="hash-placeholder text-left">
+                        {verificationHash}</p>
+                    </div>
+                </div>
+                :
+                <></>
+            }
+
+
+        </>
+    )
 
 }
