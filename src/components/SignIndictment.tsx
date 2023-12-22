caef336c81725fac130d7c85db7be3df1d0647d9
hilwyn <hilwyn@gmail.com>
2023-12-22 11:14:41 -0400

    commit

--- src/components/SignIndictment.tsx
+++ src/components/SignIndictment.tsx
@@ -908,10 +908,10 @@ knowledge;
             {!otpSent && !signed && !already_signed && !isFinalSigned ?
 
                 <div className="" style={{ display:"block", margin:"15px auto", width:"200px"}} >
-                    { acknowledged && additionalAcknowledged ?
+                    { acknowledged  ?
                         <button className="btn btn-primary" style={{  width:"200px"}}  type="submit" onClick={sendOTP}>Request Signing Code</button>
                         :
-                        <button className="btn btn-primary" style={{  width:"200px"}}  type="submit" onClick={sendOTP} disabled>Request Signing Code</button>
+                        <button className="btn btn-primary" style={{  width:"200px"}}  type="submit" onClick={sendOTP} >Request Signing Code</button>
                     }
                     
                 </div>
