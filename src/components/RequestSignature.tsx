782b04776d6c3c7a752567de4f1ef7734213fe70
Dion Santana <dion.santana@gmail.com>
2023-12-28 22:32:29 -0400

    correct clause

--- src/components/RequestSignature.tsx
+++ src/components/RequestSignature.tsx
@@ -118,8 +118,9 @@ export const RequestSignature = ({ submission_id, complainant_email }: RequestSi
                 }
             )
             console.log('Fetched submission: ', submission.data.submission)
-            console.log('indictment variable being set to: ', submission.data.submission.type == 'indictment')
-            setIndictment(submission.data.submission.type == 'indictment')
+            // console.log('indictment variable being set to: ', submission.data.submission.type == 'indictment')
+            // setIndictment(submission.data.submission.type == 'indictment')
+            setIndictment(submission?.data?.submission?.type == 'indictment')
 
         }
 
